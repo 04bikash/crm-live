5a119cb36e3c202f1852d97698c6116bea668b3d
Akash Singh <akrsingh12@gmail.com>
2021-04-07 22:08:14 +0530

    settings_updated

--- crm/settings.py
+++ crm/settings.py
@@ -96,9 +96,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
-    
 }
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
